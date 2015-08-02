17fb354bb01c3b07148e0cebc5166bf3ccd94f9c
Steven Cassidy <steven.cassidy@gmail.com>
2015-08-02 19:28:45 -0400

    Use moment.js for date display

--- js/view.js
+++ js/view.js
@@ -11,10 +11,8 @@ TodoApp.View.prototype.showTasks = function(todoList) {
     html += '  <td class="task_id">' + task.id + '</td>' ;
     html += '  <td class="task_description">' + task.description + '</td>' ;
     html += '  <td class="task_done"><input type="checkbox" id="task' + task.id + 'done">' + (task.done ? ' checked ' : '') + '</td>' ;
-    html += '  <td class="task_created_at">' + task.created_at + '</td>' ;
+    html += '  <td class="task_created_at">' + moment(task.created_at).format("MMM Do, h:mm:ss a") + '</td>' ;
     html += '</tr>' ;
   });
-  console.log(html);
   this.$tableBody.html(html);
-
 };
