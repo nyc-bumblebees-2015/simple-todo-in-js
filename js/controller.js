ed6420c23db4b5b0af67ff8e1add12cda1401f2c
Steven Cassidy <steven.cassidy@gmail.com>
2015-08-02 21:12:37 -0400

    Fix closing brace on controller

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,10 @@
 TodoApp.Controller = function(view) {
   this.list = new TodoApp.TodoList();
   this.view = view;
+}
 
 TodoApp.Controller.prototype.addTask = function(taskProps) {
   this.list.addTask(new TodoApp.Task(taskProps));
   this.view.showTasks(this.list);
-
-}
 
 }
