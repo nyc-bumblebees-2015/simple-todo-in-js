2e211414b15a5b3552fec7f8dd17f9f525fbc50d
Steven Cassidy <steven@mumsnet.com>
2015-08-03 09:07:37 -0400

    Add missing semicolon

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   var view = new TodoApp.View('table#tasks tbody');
   var controller = new TodoApp.Controller(view);
-  view.controller = controller
+  view.controller = controller;
   controller.view = view;
 
 });
